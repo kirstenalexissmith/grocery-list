eeef6ec673c8ee418a180d2a8a76015c64aa56cb
Kirsten Smith <kirstenalexis7@gmail.com>
2023-11-07 12:21:38 -0800

    add results heading

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ function handleForm(event) {
   const userSelections = document.querySelectorAll("input[name=grocery-item]:checked");
   const userSelectionsArray = Array.from(userSelections);
 
+  const resultsHeading = document.createElement("h2");
+  resultsHeading.append("You need these items from the grocery store:");
+  document.body.append(resultsHeading);
+
   userSelectionsArray.forEach(function (element) {
     const paragraph = document.createElement("p");
     paragraph.append(element.value);
